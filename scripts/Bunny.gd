c418e50e2cb582b57e8981e32fa757db3f49be1e
lanxeyu <lanxeyu@gmail.com>
2024-05-19 23:07:54 +0100

    added bunny scene and modified behaviors

--- scripts/Bunny.gd
+++ scripts/Bunny.gd
@@ -1,11 +1,26 @@
 extends CharacterBody2D
 
+@onready var ray_cast_right = $RayCastRight
+@onready var ray_cast_left = $RayCastLeft
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var direction = 1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var jump_timer = 5
+
+func _process(delta):
+
+	jump_timer -= delta
+	
+	if ray_cast_right.is_colliding():
+		direction = -1
+	if ray_cast_left.is_colliding():
+		direction = 1
+
+	position.x += direction * SPEED * delta
 
 
 func _physics_process(delta):
@@ -14,14 +29,11 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("action_up") and is_on_floor():
+	if jump_timer < 0 and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_timer = 5
 
-	# Get the input direction and handle the movement/deceleration.
-	var direction = Input.get_axis("action_left", "action_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+
 
 	move_and_slide()
+
