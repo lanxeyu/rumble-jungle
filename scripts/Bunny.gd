e5a2d0bec0475b139f0cd00055ba9143681dcd96
lanxeyu <lanxeyu@gmail.com>
2024-06-10 13:51:59 +0100

    handled Rhino animations, modified Rhino & Bunny behavior

--- scripts/Bunny.gd
+++ scripts/Bunny.gd
@@ -3,17 +3,18 @@ extends CharacterBody2D
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
 
-const SPEED = 300.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
 var direction = 1
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var jump_timer = 5
 
-func _process(delta):
-
+func _physics_process(delta):
 	jump_timer -= delta
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
 	
 	if ray_cast_right.is_colliding():
 		direction = -1
@@ -22,18 +23,10 @@ func _process(delta):
 
 	position.x += direction * SPEED * delta
 
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
 	# Handle jump.
 	if jump_timer < 0 and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		jump_timer = 5
-
-
 
 	move_and_slide()
 
--- scripts/Rhino.gd
+++ scripts/Rhino.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var ray_cast_right = $RayCastRight
+@onready var ray_cast_left = $RayCastLeft
 
-const SPEED = 300.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+var direction = 1
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -17,11 +18,27 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("action_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	var direction = Input.get_axis("action_left", "action_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if ray_cast_right.is_colliding():
+		direction = -1
+	if ray_cast_left.is_colliding():
+		direction = 1
+
+	position.x += direction * SPEED * delta
 
 	move_and_slide()
+	
+	# Handle animations
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	
+
+	
+	
