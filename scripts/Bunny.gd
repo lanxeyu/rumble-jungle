6008c4da69bbd9c581948d9247011fcc49551a62
lanxeyu <lanxeyu@gmail.com>
2024-06-13 21:22:17 +0100

    modified movement

--- scripts/Bunny.gd
+++ scripts/Bunny.gd
@@ -3,30 +3,25 @@ extends CharacterBody2D
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -400.0
+const speed = 100.0
+const jump_velocity = -400.0
 var direction = 1
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var jump_timer = 5
 
 func _physics_process(delta):
+	velocity.x = direction * speed
+	velocity.y += gravity * delta
 	jump_timer -= delta
-	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
 	
 	if ray_cast_right.is_colliding():
 		direction = -1
 	if ray_cast_left.is_colliding():
 		direction = 1
 
-	position.x += direction * SPEED * delta
-
 	# Handle jump.
 	if jump_timer < 0 and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 		jump_timer = 5
 
 	move_and_slide()
-
--- scripts/Rhino.gd
+++ scripts/Rhino.gd
@@ -4,26 +4,27 @@ extends CharacterBody2D
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -400.0
+var speed = 100.0
+var jump_velocity = -400.0
 var direction = 1
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	velocity.x = direction * speed
+	velocity.y += gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("action_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 
 	if ray_cast_right.is_colliding():
-		direction = -1
+		if !(ray_cast_right.get_collider() is CharacterBody2D):
+			print(ray_cast_right.get_collider())
+			direction = -1
 	if ray_cast_left.is_colliding():
-		direction = 1
-
-	position.x += direction * SPEED * delta
+		if !(ray_cast_left.get_collider() is CharacterBody2D):
+			print(ray_cast_left.get_collider())
+			direction = -1
 
 	move_and_slide()
 	
